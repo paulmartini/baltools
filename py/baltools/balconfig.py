bde5c94f1ee1ecde2cb4025a3e59082c5cb9c3e5
paulmartini <martini.paul@gmail.com>
2025-08-31 10:36:40 -0400

    changed default smoothing to 3 pix

--- py/baltools/balconfig.py
+++ py/baltools/balconfig.py
@@ -31,7 +31,7 @@ BI_MIN_WIDTH = 2000.        # min width for a BI trough (km/s)
 CONTINUUM_THRESHOLD = 0.9   # Flux must be below this fraction of the continuum
 ERROR_SCALING_FACTOR = 0.5  # Factor to scale error term in trough identification
 
-SMOOTHING_WIDTH = 5  # Width in pixels for boxcar smoothing before trough finding.
+SMOOTHING_WIDTH = 3  # Width in pixels for boxcar smoothing before trough finding.
 
 # --- PCA Fit Configuration ---
 
